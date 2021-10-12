2f2e61f6a1404c087c295cd62b450c420e43a98a
vdhakal <name@changeme.com>
2021-10-11 22:23:26 -0500

    ds

--- ideasFrontend/src/app/app.component.spec.ts
+++ ideasFrontend/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ideasFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ideasFrontend');
+    
   });
 
   it('should render title', () => {
---
+++ ideasFrontend/src/app/sticky-notes/sticky-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sticky-notes',
+  templateUrl: './sticky-notes.component.html',
+  styleUrls: ['./sticky-notes.component.css']
+})
+export class StickyNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
